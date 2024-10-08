40ffc40899da9d1764d1b2fb83d2462cfffcbee4
Jack Chuma <jack.chuma@coinbase.com>
2024-10-08 19:18:02 -0400

    add comments to IPrecheckContract.sol and RIP7755Structs.sol

--- contracts/src/IPrecheckContract.sol
+++ contracts/src/IPrecheckContract.sol
@@ -3,6 +3,19 @@ pragma solidity ^0.8.23;
 
 import {CrossChainCall} from "./RIP7755Structs.sol";
 
+/// @title IPrecheckContract
+///
+/// @author Coinbase (https://github.com/base-org/7755-poc)
+///
+/// @notice A standardized interface for a valid Precheck Contract compatible with RIP-7755.
+///
+/// A cross-chain-call can optionally specify a Precheck Contract used to verify some arbitrary fill condition during the `fulfill` transaction.
+/// To specify a Precheck contract, set its address in `CrossChainCall.precheckContract`.
+/// In order for the cross chain call to succeed with a precheck, the Precheck contract must inherit this interface and implement `precheckCall`.
 interface IPrecheckContract {
+    /// @notice A precheck function declaration.
+    ///
+    /// @param crossChainCall A cross chain call request formatted following the RIP-7755 spec. See {RIP7755Structs-CrossChainCall}.
+    /// @param caller The address of the filler account that submitted the transaction to RIP7755Verifier.
     function precheckCall(CrossChainCall calldata crossChainCall, address caller) external;
 }
--- contracts/src/RIP7755Structs.sol
+++ contracts/src/RIP7755Structs.sol
@@ -1,43 +1,45 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
+/// @notice Low-level call specs representing the desired transaction on destination chain
 struct Call {
-    // The address to call
+    /// @dev The address to call
     address to;
-    // The calldata to call with
+    /// @dev The calldata to call with
     bytes data;
-    // The native asset value of the call
+    /// @dev The native asset value of the call
     uint256 value;
 }
 
+/// @notice A cross chain call request formatted following the RIP-7755 spec
 struct CrossChainCall {
-    // Array of calls to make on the destination chain
+    /// @dev Array of calls to make on the destination chain
     Call[] calls;
-    // The contract on origin chain where this cross-chain call request originated
+    /// @dev The contract on origin chain where this cross-chain call request originated
     address originationContract;
-    // The chainId of the origin chain
+    /// @dev The chainId of the origin chain
     uint256 originChainId;
-    // The chainId of the destination chain
+    /// @dev The chainId of the destination chain
     uint256 destinationChainId;
-    // The nonce of this call, to differentiate from other calls with the same values
+    /// @dev The nonce of this call, to differentiate from other calls with the same values
     uint256 nonce;
-    // The L2 contract on destination chain that's storage will be used to verify whether or not this call was made
+    /// @dev The L2 contract on destination chain that's storage will be used to verify whether or not this call was made
     address verifyingContract;
-    // The L1 address of the contract that should have L2 block info stored
+    /// @dev The L1 address of the contract that should have L2 block info stored
     address l2Oracle;
-    // The storage key at which we expect to find the L2 block info on the l2Oracle
+    /// @dev The storage key at which we expect to find the L2 block info on the l2Oracle
     bytes32 l2OracleStorageKey;
-    // The address of the ERC20 reward asset to be paid to whoever proves they filled this call
-    // Native asset specified as in ERC-7528 format
+    /// @dev The address of the ERC20 reward asset to be paid to whoever proves they filled this call
+    /// @dev Native asset specified as in ERC-7528 format
     address rewardAsset;
-    // The reward amount to pay
+    /// @dev The reward amount to pay
     uint256 rewardAmount;
-    // The minimum age of the L1 block used for the proof
+    /// @dev The minimum age of the L1 block used for the proof
     uint256 finalityDelaySeconds;
-    // An optional pre-check contract address on the destination chain
-    // Zero address represents no pre-check contract desired
-    // Can be used for arbitrary validation of fill conditions
+    /// @dev An optional pre-check contract address on the destination chain
+    /// @dev Zero address represents no pre-check contract desired
+    /// @dev Can be used for arbitrary validation of fill conditions
     address precheckContract;
-    // Arbitrary encoded precheck data
+    /// @dev Arbitrary encoded precheck data
     bytes precheckData;
 }
