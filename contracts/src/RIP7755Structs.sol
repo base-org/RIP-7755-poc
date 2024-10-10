ced0cf0993a9db5646231383482adbca112ba48c
Jack Chuma <jack.chuma@coinbase.com>
2024-10-10 16:38:51 -0400

    use OZ Address library in verifier contract and solc v0.8.24

--- contracts/src/RIP7755Structs.sol
+++ contracts/src/RIP7755Structs.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.23;
+pragma solidity 0.8.24;
 
 /// @notice Low-level call specs representing the desired transaction on destination chain
 struct Call {
--- contracts/test/mocks/MockPrecheck.sol
+++ contracts/test/mocks/MockPrecheck.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.23;
+pragma solidity 0.8.24;
 
 import {IPrecheckContract} from "../../src/IPrecheckContract.sol";
 import {CrossChainCall} from "../../src/RIP7755Structs.sol";
