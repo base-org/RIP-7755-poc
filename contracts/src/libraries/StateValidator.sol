2cf99987d8b517c5996d53e770f86f5d8ff077bb
Jack Chuma <jack.chuma@coinbase.com>
2024-10-11 15:12:47 -0400

    add comments to StateValidator

--- contracts/src/libraries/StateValidator.sol
+++ contracts/src/libraries/StateValidator.sol
@@ -7,32 +7,77 @@ import {SecureMerkleTrie} from "optimism/packages/contracts-bedrock/src/librarie
 
 import {SSZ} from "./SSZ.sol";
 
+/// @title StateValidator
+///
+/// @author Coinbase (https://github.com/base-org/RIP-7755-poc)
+///
+/// @notice A library for validating EVM storage proofs.
 library StateValidator {
     using RLPReader for RLPReader.RLPItem;
     using RLPReader for bytes;
 
+    /// @notice The Beacon Roots Oracle contract on L2. Ethereum's Beacon Chain publishes beacon roots here as explained
+    /// by EIP-4788 (https://eips.ethereum.org/EIPS/eip-4788)
     address private constant BEACON_ROOTS_ORACLE = 0x000F3df6D732807Ef1319fB7B8bB8522d0Beac02;
+
+    /// @notice g-index stands for Generalized Index - a way to define the position of a node in a merkle tree. This
+    /// represents the position of the execution client's state root in the merkle tree producing the beacon root.
     uint256 private constant STATE_ROOT_GINDEX = 6434;
 
+    /// @notice Parameters needed to validate the authenticity of Ethereum's execution client's state root
     struct StateProofParameters {
+        /// @dev The Beacon Chain root published to `BEACON_ROOTS_ORACLE` on this L2 chain
         bytes32 beaconRoot;
+        /// @dev The timestamp associated with the provided Beacon Root
         uint256 beaconOracleTimestamp;
+        /// @dev The state root of Ethereum's execution client
         bytes32 executionStateRoot;
+        /// @dev A proof to verify the authenticity of `executionStateRoot`
         bytes32[] stateRootProof;
     }
 
+    /// @notice Parameters needed to validate the authenticity of an EVM account's storage
     struct AccountProofParameters {
+        /// @dev The storage location to validate
         bytes storageKey;
+        /// @dev The expected value at the specified storage location
         bytes storageValue;
+        /// @dev A proof used to derive an account's storage root
         bytes[] accountProof;
+        /// @dev A proof to validate the account's `storageValue` at `storageKey` location
         bytes[] storageProof;
     }
 
+    /// @notice This error is thrown when the passed in beacon root does not match what is stored in
+    /// `BEACON_ROOTS_ORACLE` for the associated timestamp
+    /// @param expected The beacon root passed in by the fulfiller
+    /// @param actual The stored beacon root in `BEACON_ROOTS_ORACLE`
     error BeaconRootDoesNotMatch(bytes32 expected, bytes32 actual);
+
+    /// @notice This error is thrown when the `staticcall` to `BEACON_ROOTS_ORACLE` does not complete successfully
+    /// @param callData The encoded timestamp that was passed in as the calldata in the staticcall to `BEACON_ROOTS_ORACLE`
     error BeaconRootsOracleCallFailed(bytes callData);
+
+    /// @notice This error is thrown when validation of the execution client's state root fails
     error ExecutionStateRootMerkleProofFailed();
+
+    /// @notice This error is thrown when the provided `accountProof` does not successfully produce an RLP-encoded account
     error AccountProofVerificationFailed();
 
+    /// @notice This error is thrown when the RLP-encoded account object returned from
+    /// `AccountProofParameters.accountProof` is formatted incorrectly
+    error InvalidAccountRLP();
+
+    /// @notice Validates the state of an EVM account at a specified storage location
+    ///
+    /// @dev First confirms a valid Beacon root and a valid execution state root
+    ///
+    /// @param account An EVM account - in most cases, either an L2Oracle contract on Eth Mainnet or `RIP7755Verifier`
+    /// on a destination L2
+    /// @param stateProofParams Parameters needed to validate the authenticity of Ethereum's execution client's state root
+    /// @param accountProofParams Parameters needed to validate the authenticity of an EVM account's storage
+    ///
+    /// @return _ True if proof validation succeeds, else false
     function validateState(
         address account,
         StateProofParameters memory stateProofParams,
@@ -50,19 +95,30 @@ library StateValidator {
         return validateAccountStorage(account, stateProofParams.executionStateRoot, accountProofParams);
     }
 
+    /// @notice Validates the state of an EVM account at a specified storage location
+    ///
+    /// @dev This is intended to ONLY be called if the authenticity of the beacon root and execution client state root
+    /// has already been verified
+    ///
+    /// @param account An EVM account - in most cases, either an L2Oracle contract on Eth Mainnet or `RIP7755Verifier`
+    /// on a destination L2
+    /// @param stateRoot The state root of an EVM chain's execution client
+    /// @param accountProofParams Parameters needed to validate the authenticity of an EVM account's storage
+    ///
+    /// @return _ True if proof validation succeeds, else false
     function validateAccountStorage(
         address account,
         bytes32 stateRoot,
         AccountProofParameters memory accountProofParams
     ) internal pure returns (bool) {
-        // 3. Validate L1 account proof where `account` here is the destination chain's inbox contract
+        // Derive the account key that shows up in the execution client's merkle trie
         bytes memory accountKey = abi.encodePacked(keccak256(abi.encodePacked(account)));
+        // Use the account proof to derive the RLP-encoded account metadata
         bytes memory encodedAccount = _verifyAccountProof(accountKey, accountProofParams.accountProof, stateRoot);
 
         // Extract storage root from account data
         bytes32 storageRoot = _extractStorageRoot(encodedAccount);
 
-        // 4. Validate storage proof proving destination L2 root stored in L1 inbox contract
         return _verifyStorageProof({
             storageKey: accountProofParams.storageKey,
             expectedStorageValue: accountProofParams.storageValue,
@@ -71,6 +127,14 @@ library StateValidator {
         });
     }
 
+    /// @notice Confirms the received beacon root is valid and associated with the received beacon oracle timestamp
+    ///
+    /// @custom:reverts If the staticcall to `BEACON_ROOTS_ORACLE` fails
+    /// @custom:reverts If the beacon root associated with `timestamp` in `BEACON_ROOTS_ORACLE` does not match the
+    /// provided root
+    ///
+    /// @param root The Beacon Chain root posted in `BEACON_ROOTS_ORACLE` on this L2
+    /// @param timestamp The timestamp associated with when the beacon root was posted
     function _checkValidBeaconRoot(bytes32 root, uint256 timestamp) private view {
         (bool success, bytes memory result) = BEACON_ROOTS_ORACLE.staticcall(abi.encode(timestamp));
 
@@ -85,15 +149,34 @@ library StateValidator {
         }
     }
 
+    /// @notice Confirms the authenticity of the passed in execution client state root
+    ///
+    /// @custom:reverts If the proof does not successfully verify the execution state root
+    ///
+    /// @param beaconRoot The Beacon Chain root posted in `BEACON_ROOTS_ORACLE` on this L2
+    /// @param executionStateRoot The state root of Ethereum's execution client
+    /// @param proof A proof to verify the authenticity of `executionStateRoot`
     function _checkValidStateRoot(bytes32 beaconRoot, bytes32 executionStateRoot, bytes32[] memory proof)
         private
         view
     {
-        if (!SSZ.verifyProof({proof: proof, root: beaconRoot, leaf: executionStateRoot, index: STATE_ROOT_GINDEX})) {
+        bool isValid =
+            SSZ.verifyProof({proof: proof, root: beaconRoot, leaf: executionStateRoot, index: STATE_ROOT_GINDEX});
+
+        if (!isValid) {
             revert ExecutionStateRootMerkleProofFailed();
         }
     }
 
+    /// @notice Using a provided account proof, derives an RLP-encoded account object containing evm account metadata
+    ///
+    /// @custom:reverts If no encoded account is found in the merkle trie
+    ///
+    /// @param accountKey The account key that shows up in the execution client's merkle trie
+    /// @param accountProof A proof used to derive an account's storage root
+    /// @param stateRoot The state root of Ethereum's execution client
+    ///
+    /// @return _ An RLP-encoded account object
     function _verifyAccountProof(bytes memory accountKey, bytes[] memory accountProof, bytes32 stateRoot)
         private
         pure
@@ -106,12 +189,31 @@ library StateValidator {
         return encodedAccount;
     }
 
+    /// @notice Extracts the storage root from an RLP-encoded account object
+    ///
+    /// @custom:reverts If the encoded account doesn't parse into 4 `RLPReader.RLPItem`s
+    ///
+    /// @param encodedAccount An RLP-encoded account object
+    ///
+    /// @return _ The account's storage root
     function _extractStorageRoot(bytes memory encodedAccount) private pure returns (bytes32) {
         RLPReader.RLPItem[] memory accountFields = encodedAccount.readList();
-        require(accountFields.length == 4, "Invalid account RLP");
-        return bytes32(RLPReader.readBytes(accountFields[2])); // storage root is the third field
+
+        if (accountFields.length != 4) {
+            revert InvalidAccountRLP();
+        }
+
+        return bytes32(accountFields[2].readBytes()); // storage root is the third field
     }
 
+    /// @notice Verifies an account's storage value at a specified location
+    ///
+    /// @param storageKey The storage location to validate
+    /// @param expectedStorageValue The expected value stored at `storageKey`
+    /// @param storageRoot The storage root for the account's merkle trie
+    /// @param storageProof The proof that validates the storage value
+    ///
+    /// @return _ True if proof validation succeeds, else false
     function _verifyStorageProof(
         bytes memory storageKey,
         bytes memory expectedStorageValue,
@@ -119,7 +221,7 @@ library StateValidator {
         bytes[] memory storageProof
     ) private pure returns (bool) {
         bytes memory rlpValue = SecureMerkleTrie.get({_key: storageKey, _proof: storageProof, _root: storageRoot});
-        bytes memory value = RLPReader.readBytes(rlpValue);
+        bytes memory value = rlpValue.readBytes();
         return keccak256(value) == keccak256(expectedStorageValue);
     }
 }
