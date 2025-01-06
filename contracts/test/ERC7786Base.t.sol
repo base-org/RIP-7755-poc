9f5d1f287a79d3e079b5d07da87c0ecdd76a9e6e
Jack Chuma <jack.chuma@coinbase.com>
2025-01-06 14:29:14 -0500

    add testing for erc7786base contract

---
+++ contracts/test/ERC7786Base.t.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {BaseTest} from "./BaseTest.t.sol";
+
+contract ERC7786BaseTest is BaseTest {
+    function test_locateAttribute_returnsAttribute() external view {
+        bytes[] memory attributes = new bytes[](1);
+        attributes[0] = abi.encodeWithSelector(_FULFILLER_ATTRIBUTE_SELECTOR, FILLER);
+        bytes memory returnedAttribute = this.submitAttributes(attributes, _FULFILLER_ATTRIBUTE_SELECTOR);
+        assertEq(returnedAttribute, attributes[0]);
+    }
+
+    function test_locateAttribute_revertsIfAttributeNotFound() external {
+        bytes[] memory attributes = new bytes[](1);
+        attributes[0] = abi.encodeWithSelector(_FULFILLER_ATTRIBUTE_SELECTOR, FILLER);
+        vm.expectRevert(abi.encodeWithSelector(AttributeNotFound.selector, _PRECHECK_ATTRIBUTE_SELECTOR));
+        this.submitAttributes(attributes, _PRECHECK_ATTRIBUTE_SELECTOR);
+    }
+
+    function test_locateAttributeUnchecked_returnsAttribute() external view {
+        bytes[] memory attributes = new bytes[](1);
+        attributes[0] = abi.encodeWithSelector(_FULFILLER_ATTRIBUTE_SELECTOR, FILLER);
+        (bool found, bytes memory returnedAttribute) =
+            this.locateAttributeUnchecked(attributes, _FULFILLER_ATTRIBUTE_SELECTOR);
+        assertTrue(found);
+        assertEq(returnedAttribute, attributes[0]);
+    }
+
+    function test_locateAttributeUnchecked_returnsFalseIfAttributeNotFound() external view {
+        bytes[] memory attributes = new bytes[](1);
+        attributes[0] = abi.encodeWithSelector(_FULFILLER_ATTRIBUTE_SELECTOR, FILLER);
+        (bool found, bytes memory returnedAttribute) =
+            this.locateAttributeUnchecked(attributes, _PRECHECK_ATTRIBUTE_SELECTOR);
+        assertFalse(found);
+        assertEq(returnedAttribute, attributes[0]);
+    }
+
+    function submitAttributes(bytes[] calldata attributes, bytes4 selector) public pure returns (bytes calldata) {
+        return _locateAttribute(attributes, selector);
+    }
+
+    function locateAttributeUnchecked(bytes[] calldata attributes, bytes4 selector)
+        public
+        pure
+        returns (bool, bytes calldata)
+    {
+        return _locateAttributeUnchecked(attributes, selector);
+    }
+}
